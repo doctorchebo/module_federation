672e25c1826654766cf96abf78d386474b7d63d2
Eisen Havale <eisenprogramando@gmail.com>
2022-06-22 08:09:52 -0500

    Micro frontend static already added to the whole app

--- tracking-tool-webapp/src/pages/login/index.jsx
+++ tracking-tool-webapp/src/pages/login/index.jsx
@@ -1,8 +1,5 @@
-import React from 'react';
-// import { Grid, Header, Image, Segment } from 'semantic-ui-react';
-// import locale from './locale/en.json';
-// import LoginView from './views';
 import { useHistory } from 'react-router';
+import React, { Suspense } from 'react';
 import './index.css';
 import { useApplication } from 'application/context/AppContext';
 
@@ -12,32 +9,19 @@ const RemoteApp = React.lazy(() => import('app2/App'));
  * @returns {React.Component} -
  */
 export default function Login() {
-	const [state] = useApplication();
+	const [state] = useApplication(); //TODO connect with the login logic
 	const history = useHistory();
 
 	/**
 	 *
 	 */
-	function toProfile() {
+	function toProfile() { //TODO connect with the login logic
 		history.push('/dashboard');
 	}
 
 	return (
-		<div className='login-container'>
-			{/* <Grid verticalAlign={'top'} className='login'>
-				<Segment basic className='personal-loading' loading={state.signIn} />
-				<Grid.Column width={10}>
-					<Image src={locale.brand} />
-				</Grid.Column>
-				<Grid.Column width={6} className='login-form'>
-					<div className='form-data'>
-						<Header as='h2'>{locale.welcome}</Header>
-						<br />
-						<LoginView redirectToProfile={toProfile} />
-					</div>
-				</Grid.Column>
-			</Grid> */}
+		<Suspense fallback={'loading...'}>
 			<RemoteApp />
-		</div>
+		</Suspense>
 	);
 }
